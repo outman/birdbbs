38579a2231f935d71795010628f7b1c17dedcd7f
xbugs <xbugs@qq.com>
2014-01-19 19:12:50 +0800

    增加Google统计

--- www/themes/classic/views/layouts/layout.php
+++ www/themes/classic/views/layouts/layout.php
@@ -27,5 +27,14 @@ $version = "?20140114";
     <hr>
     <p>&copy; <?php echo date("Y"), '. <a href="http://buxiangshuo.cn">不想说网</a> ', Yii::powered(); ?></p>
 </div>
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-15506010-4', 'buxiangshuo.cn');
+ga('send', 'pageview');
+</script>
 </body>
 </html>
