7564fea301c0372009077a2a7e032564c6814dfb
xbugs <xbugs@qq.com>
2014-01-21 23:24:50 +0800

    替换皮肤

--- www/themes/classic/views/layouts/front.php
+++ www/themes/classic/views/layouts/front.php
@@ -1,5 +1,5 @@
 <?php $this->beginContent('//layouts/layout'); ?>
-<div class="navbar navbar-fixed-top navbar-inverse" role="navigation">
+<div class="navbar navbar-fixed-top navbar-default" role="navigation">
     <div class="container">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -15,14 +15,17 @@
                 <li class="active"><a href="<?php echo Yii::app()->createUrl("home/index") ?>">首页</a></li>
                 <?php $this->widget('NodeNavWidget'); ?>
             </ul>
-            <?php if (!Yii::app()->user->isGuest): ?>
             <ul class="nav navbar-nav pull-right">
+                <?php if (Yii::app()->user->isGuest): ?>
+                <li><a href="<?php echo Yii::app()->createUrl("home/login"); ?>">登录</a></li>
+                <li><a href="<?php echo Yii::app()->createUrl("home/register"); ?>">注册</a></li>
+                <?php else: ?>
                 <li class="active">
                     <a href="<?php echo Yii::app()->createUrl("home/user"); ?>"><?php echo CHtml::encode(Yii::app()->user->name); ?></a>
                 </li>
                 <li><a href="<?php echo Yii::app()->createUrl("home/logout") ?>">退出</a></li>
+                <?php endif; ?>
             </ul>
-            <?php endif; ?>
         </div>
     </div>
 </div>
